81ad56e7975ea916ff1446e0d900331b437478c9
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-05-20 15:00:01 +0200

    fix

--- esercizio-1/main.js
+++ esercizio-1/main.js
@@ -11,11 +11,11 @@ function controllo(){
     if((number%3 == 0) && (number%5 == 0)){
         return "FizzBuzz"
 
-    }else if(number%5 == 0){
-        return "Buzz"
-
     }else if(number%3 == 0){
         return "Fizz"
+    
+    }else if(number%5 == 0){
+        return "Buzz"
 
     }else if((number%4 !=0) && (number%5 !=0)){
         return number
--- esercizio-2/main.js
+++ esercizio-2/main.js
@@ -9,16 +9,12 @@ let parolaDivisa = parola.split('');
 let parolaInvertita = [];
 
 function controllo(){
-    for(i=0; i<parolaDivisa.length; i++){
-
-    }
-
     for(let i=parolaDivisa.length -1; i>=0; i--){
         parolaInvertita.push(parolaDivisa[i]);
     }
 
-
     console.log(parolaDivisa, parolaInvertita);
+
 
     if(parolaDivisa.toString()==parolaInvertita.toString()){
         return true
